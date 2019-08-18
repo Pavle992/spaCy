6176d84113eca8e79ce413bead1482358e3f2fd1
Pavle992 <pavle.vidanovic@gmail.com>
2019-08-18 08:16:09 +0200

    Tokenizer exceptions updated. Norm exception added.

--- spacy/lang/rs/tokenizer_exceptions.py
+++ spacy/lang/rs/tokenizer_exceptions.py
@@ -28,7 +28,6 @@ _abbrev_exc = [
     {ORTH: "феб", LEMMA: "фебруар", NORM: "фебруар"},
     {ORTH: "мар", LEMMA: "март", NORM: "март"},
     {ORTH: "апр", LEMMA: "април", NORM: "април"},
-    {ORTH: "мај", LEMMA: "мај", NORM: "мај"},
     {ORTH: "јуни", LEMMA: "јун", NORM: "јун"},
     {ORTH: "јули", LEMMA: "јул", NORM: "јул"},
     {ORTH: "авг", LEMMA: "август", NORM: "август"},
@@ -36,7 +35,7 @@ _abbrev_exc = [
     {ORTH: "септ", LEMMA: "септембар", NORM: "септембар"},
     {ORTH: "окт", LEMMA: "октобар", NORM: "октобар"},
     {ORTH: "нов", LEMMA: "новембар", NORM: "новембар"},
-    {ORTH: "дец", LEMMA: "децембар", NORM: "децембар"},
+    {ORTH: "дец", LEMMA: "децембар", NORM: "децембар"}
 ]
 
 
@@ -51,9 +50,11 @@ for abbrev_desc in _abbrev_exc:
 _slang_exc = [
     # without dot
     {ORTH: 'др', LEMMA: 'доктор', NORM: 'доктор'},
+    {ORTH: 'гдин', LEMMA: 'господин', NORM: 'господин'},
     {ORTH: 'гђа', LEMMA: 'госпођа', NORM: 'госпођа'},
+    {ORTH: 'гђица', LEMMA: 'госпођица', NORM: 'госпођица'},
     {ORTH: 'мр', LEMMA: 'магистар', NORM: 'магистар'},
-    {ORTH: 'Бгд', LEMMA: 'Београд', NORM: 'Београд'},
+    {ORTH: 'Бгд', LEMMA: 'Београд', NORM: 'београд'},
     {ORTH: 'цм', LEMMA: 'центиметар', NORM: 'центиметар'},
     {ORTH: 'м', LEMMA: 'метар', NORM: 'метар'},
     {ORTH: 'км', LEMMA: 'километар', NORM: 'километар'},
@@ -67,30 +68,35 @@ _slang_exc = [
     {ORTH: 'нпр.', LEMMA: 'на пример', NORM: 'на пример'},
     {ORTH: 'тзв.', LEMMA: 'такозван', NORM: 'такозван'},
     {ORTH: 'проф.', LEMMA: 'професор', NORM: 'професор'},
-    {ORTH: 'итд.', LEMMA: ' и тако даље', NORM: 'и тако даље'},
     {ORTH: 'стр.', LEMMA: 'страна', NORM: 'страна'},
     {ORTH: 'једн.', LEMMA: 'једнина', NORM: 'једнина'},
     {ORTH: 'мн.', LEMMA: 'множина', NORM: 'множина'},
     {ORTH: 'уч.', LEMMA: 'ученик', NORM: 'ученик'},
     {ORTH: 'разр.', LEMMA: 'разред', NORM: 'разред'},
-    {ORTH: 'н.е.', LEMMA: 'нова ера', NORM: 'нове ере'},
-    {ORTH: 'о.м.', LEMMA: 'овај месец', NORM: 'децилитар'},
     {ORTH: 'инж.', LEMMA: 'инжењер', NORM: 'инжењер'},
     {ORTH: 'гимн.', LEMMA: 'гимназија', NORM: 'гимназија'},
-    {ORTH: 'уч.', LEMMA: 'милиграм', NORM: 'милиграм'},
-    {ORTH: 'разр.', LEMMA: 'килограм', NORM: 'килограм'},
-    {ORTH: 'стр.', LEMMA: 'децилитар', NORM: 'децилитар'},
     {ORTH: 'год.', LEMMA: 'година', NORM: 'година'},
     {ORTH: 'мед.', LEMMA: 'медицина', NORM: 'медицина'},
     {ORTH: 'гимн.', LEMMA: 'гимназија', NORM: 'гимназија'},
-    {ORTH: "акад.", LEMMA: "академік", NORM: "академік"},
-    {ORTH: "доц.", LEMMA: "доцент", NORM: "доцент"}
+    {ORTH: "акад.", LEMMA: "академик", NORM: "академик"},
+    {ORTH: "доц.", LEMMA: "доцент", NORM: "доцент"},
+    {ORTH: "итд.", LEMMA: "и тако даље", NORM: "и тако даље"},
+    {ORTH: "и сл.", LEMMA: "и слично", NORM: "и слично"},
+    {ORTH: "н.е.", LEMMA: "нова ера", NORM: "нова ера"},
+    {ORTH: "о.г.", LEMMA: "ова година", NORM: "ова година"},
+    {ORTH: "л.к.", LEMMA: "лична карта", NORM: "лична карта"},
+    {ORTH: "в.д.", LEMMA: "вршилац дужности", NORM: "вршилац дужности"},
+    {ORTH: "стр.", LEMMA: "страна", NORM: "страна"},
+    # with qoute
+    {ORTH: "ал'", LEMMA: "али", NORM: "али"},
+    {ORTH: "ил'", LEMMA: "или", NORM: "или"},
+    {ORTH: "је л'", LEMMA: "је ли", NORM: "је ли"},
+    {ORTH: "да л'", LEMMA: "да ли", NORM: "да ли"},
+    {ORTH: "држ'те", LEMMA: "држати", NORM: "држите"}
 ]
 
 for slang_desc in _slang_exc:
     _exc[slang_desc[ORTH]] = [slang_desc]
 
-
-TOKENIZER_EXCEPTIONS = _exc
 
 TOKENIZER_EXCEPTIONS = _exc
